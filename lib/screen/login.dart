1234d6034baa9cf86969228a5a77cb24b372927b
yadavendras <yadavendra.singh@in.cpm-int.com>
2018-04-27 12:03:59 +0530

    ysy

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 class Login extends StatelessWidget {
-
   static final TextEditingController _uid = new TextEditingController();
   static final TextEditingController _psw = new TextEditingController();
 
@@ -16,65 +16,89 @@ class Login extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         body: new Center(
-            child: new ListView(
-                shrinkWrap: true,
-                children: <Widget>[
-                  new Container(
-                    margin: new EdgeInsets.symmetric(vertical: 20.0),
-                    child: new Center(
-                      child: new Image(image: new AssetImage(
-                          'assets/cpm_10th_year_celebration.png'),
-                        height: 150.0,
-                        width: 150.0,),
-                    ),
-                  ),
-                  new SizedBox(height: 24.0),
-                  new Container(
-                    margin: new EdgeInsets.symmetric(vertical: 10.0),
-                    height: 200.0,
-                    child: new Center(
-                      child: new Card(
-                        child: new Container(
-                          padding: new EdgeInsets.all(15.0),
-                          child: new Column(
-                            children: <Widget>[
-                              new TextField(controller: _uid,
-                                decoration: new InputDecoration(
-                                    hintText: "Enter User Id"),),
-                              new TextField(controller: _psw,
-                                decoration: new InputDecoration(
-                                    hintText: "Enter Password"),
-                                obscureText: true,),
-                              new Container(
-                                margin: new EdgeInsets.symmetric(
-                                    vertical: 10.0),
-                                child: new RaisedButton(
-                                    child: new Text("Submit"), onPressed: () {
-                                  //Navigator.of(context).pushNamed('/Second');
-                                  Navigator.of(context).pushNamed('/MainPage');
-                                }),
-                              )
-                            ],
-                          ),
-                        ),
+            child: new ListView(shrinkWrap: true, children: <Widget>[
+          new Container(
+            margin: new EdgeInsets.symmetric(vertical: 20.0),
+            child: new Center(
+              child: new Image(
+                image: new AssetImage('assets/cpm_10th_year_celebration.png'),
+                height: 150.0,
+                width: 150.0,
+              ),
+            ),
+          ),
+          new SizedBox(height: 24.0),
+          new Container(
+            margin: new EdgeInsets.symmetric(vertical: 10.0),
+            height: 200.0,
+            child: new Center(
+              child: new Form(
+                  child: new Card(
+                child: new Container(
+                  padding: new EdgeInsets.all(15.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new TextFormField(
+                        controller: _uid,
+                        decoration:
+                            new InputDecoration(hintText: "Enter User Id"),
+                        validator: (val) =>
+                        val.length < 1 ? 'Please enter User Id.' : null,
                       ),
-                    ),
+                      new TextFormField(
+                        controller: _psw,
+                        decoration:
+                            new InputDecoration(hintText: "Enter Password"),
+                        obscureText: true,
+                        validator: (val) =>
+                        val.length < 1 ? 'Please enter password.' : null,
+                      ),
+                      new Container(
+                        margin: new EdgeInsets.symmetric(vertical: 10.0),
+                        child: new RaisedButton(
+                            child: new Text("Submit"),
+                            onPressed: () {
+                              //Navigator.of(context).pushNamed('/Second');
+                              _onLoading(context);
+                            }),
+                      )
+                    ],
                   ),
-                  new SizedBox(height: 24.0),
-                  new Container(
-                    margin: new EdgeInsets.symmetric(vertical: 10.0),
-                    child: new Center(
-                      child: new Text("(c) All rights reserved 2018"),
-                    ),
-                  ),
-                ]
-            )
-        )
-    );
+                ),
+              )),
+            ),
+          ),
+          new SizedBox(height: 24.0),
+          new Container(
+            margin: new EdgeInsets.symmetric(vertical: 10.0),
+            child: new Center(
+              child: new Text("(c) All rights reserved 2018"),
+            ),
+          ),
+        ])));
   }
 
   void onSubmit() {
     print("Login with: " + _uid.text + " " + _psw.text);
   }
 
+  void _onLoading(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      child: new Dialog(
+        child: new Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            new CircularProgressIndicator(),
+            new Text("Loading"),
+          ],
+        ),
+      ),
+    );
+    new Future.delayed(new Duration(seconds: 3), () {
+      Navigator.pop(context); //pop dialog
+      Navigator.of(context).pushNamed('/MainPage');
+    });
+  }
 }
